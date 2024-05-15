e87a05e38830aa13137f4977c80d813980677b08
Justin Navarro <info@justinnavarro.net>
2024-05-15 06:21:15 -0400

    initial commit

---
+++ src/main/kotlin/com/jnav/Application.kt
@@ -0,0 +1,14 @@
+package com.jnav
+
+import com.jnav.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureSerialization()
+    configureRouting()
+}
