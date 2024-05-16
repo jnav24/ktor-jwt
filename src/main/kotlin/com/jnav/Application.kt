4c773e271b06b6c7acd787ab0e4565a0f1331440
Justin Navarro <info@justinnavarro.net>
2024-05-16 05:15:30 -0400

    configure the router and add endpoints

--- src/main/kotlin/com/jnav/Application.kt
+++ src/main/kotlin/com/jnav/Application.kt
@@ -1,6 +1,9 @@
 package com.jnav
 
 import com.jnav.plugins.configureSerialization
+import com.jnav.repository.UserRepository
+import com.jnav.routing.configureRouting
+import com.jnav.service.UserService
 import io.ktor.server.application.*
 
 fun main(args: Array<String>) {
@@ -8,5 +11,9 @@ fun main(args: Array<String>) {
 }
 
 fun Application.module() {
+    val userRepository = UserRepository()
+    val userService = UserService(userRepository)
+
     configureSerialization()
+    configureRouting(userService)
 }
---
+++ src/main/kotlin/com/jnav/routing/UserRoute.kt
@@ -0,0 +1,60 @@
+package com.jnav.routing
+
+import com.jnav.model.User
+import com.jnav.routing.request.UserRequest
+import com.jnav.routing.response.UserResponse
+import com.jnav.service.UserService
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import java.util.*
+
+fun Route.userRoute(
+    userService: UserService,
+) {
+    post {
+        val userRequest = call.receive<UserRequest>()
+
+        val createdUser = userService.save(
+            user = userRequest.toModel()
+        ) ?: return@post call.respond(HttpStatusCode.BadRequest)
+
+        call.response.header(
+            name = "id",
+            value = createdUser.id.toString()
+        )
+
+        call.respond(message = HttpStatusCode.Created)
+    }
+
+    get {
+        val users = userService.findAll()
+
+        call.respond(message = users.map(User::toResponse))
+    }
+
+    get("/{id}") {
+        val id: String = call.parameters["id"]
+            ?: return@get call.respond(HttpStatusCode.BadRequest)
+
+        val foundUser = userService.findById(id)
+            ?: return@get call.respond(HttpStatusCode.NotFound)
+
+        call.respond(message = foundUser.toResponse())
+    }
+}
+
+private fun UserRequest.toModel(): User =
+    User(
+        id = UUID.randomUUID(),
+        username = this.username,
+        password = this.password,
+    )
+
+private fun User.toResponse(): UserResponse =
+    UserResponse(
+        id = this.id,
+        username = this.username,
+    )
